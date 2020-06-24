ee659cdeefe43d3fe16af8b9b4f479f9f4b8b345
couettou <delignyb.pro@gmail.com>
2020-06-24 09:25:20 +0200

    spring init for Hackathon

---
+++ src/main/java/com/hackathon/GoogleFirstResultBOOMBack/GoogleFirstResultBoomBackApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.GoogleFirstResultBOOMBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleFirstResultBoomBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleFirstResultBoomBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/GoogleFirstResultBOOMBack/GoogleFirstResultBoomBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.GoogleFirstResultBOOMBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleFirstResultBoomBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
